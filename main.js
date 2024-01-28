b09d0f27c0fd6b582a7c4403908e3fa3be889edf
DimasMukhlasIbrahim <131153846+DimasMukhlasIbrahim@users.noreply.github.com>
2024-01-28 07:49:43 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// const input =document.getElementById ('input-teks')
+// const btn = document.getElementById ('btn-kalkulator')
+// console.log (btn)
+// btn[0].addEventListener ('click', () => {
+//     console.log ('hai')
+// })
